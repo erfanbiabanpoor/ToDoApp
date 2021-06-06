f2d565a543943b256e00c1027eff06d366aa9a51
erfan <erfanbiabanpoor@yahoo.com>
2021-06-06 19:41:59 +0430

    almost done

--- todo.js
+++ todo.js
@@ -1,7 +1,18 @@
-function add() {
-    let item = document.getElementById("input").value;
-    if(text == "" || isNaN(text) !== true)
-    {
-        alert("Input is not unacceptable");
-    }
-}
+    function check() {
+        let item = document.getElementById("input").value;
+        if(item == "" || isNaN(item) !== true){
+            alert(" input your task ")
+        }
+    } 
+    
+    let input = document.getElementById("button");
+    input.addEventListener("click",addtask);
+
+        function addtask() {
+            let item = document.getElementById("input").value;
+            let lst = document.createElement("lst");
+            let text = document.createTextNode(item);
+            lst.appendChild(text);
+            let ul = document.getElementById('list');
+            ul.appendChild(lst);
+        }
