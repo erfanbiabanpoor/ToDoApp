c296050318fd2dca5ea9d40e761a7102d8a5cd78
erfan <erfanbiabanpoor@yahoo.com>
2021-09-17 20:31:14 +0430

    dooooooooooone

--- model.js
+++ model.js
@@ -1,26 +1,20 @@
-class Model{
-    constructor() {
-        this.toDoList = [];
-    }
+class Model {
+  constructor() {
+    this.toDoList = [];
+  }
 
-    addTodo(value){
-        this.toDoList.push(value);
+  addTodo(value) {
+    this.toDoList.push(value);
+  }
+  deleteTodo(value) {
+    var i = 0;
+    while (i < this.toDoList.length) {
+      if (this.toDoList[i] === value) {
+        arr.splice(i, 1);
+      } else {
+        ++i;
+      }
     }
-    deleteTodo(value)
-    {
-
-         var i = 0;
-        while (i < this.toDoList.length) 
-            {
-            if (this.toDoList[i] === value) 
-            {
-                arr.splice(i,1);
-            }
-        else
-            {
-                ++i;
-            }
-        }
-    }
+  }
 }
 export default Model;
--- view.js
+++ view.js
@@ -1,77 +1,74 @@
-class View {    
-    constructor(){
-        let taskInput = document.getElementById("new-task");
-        let addButton = document.getElementsByTagName("button")[0];
-        let incompleteTaskHolder = document.getElementById("incomplete-tasks");
-        let completedTasksHolder = document.getElementById("completed-tasks");
-    }
-    AddTask(TaskTitle) 
-    {
-        let createNewTaskElement=function(TaskTitle){
+class View {
+  constructor() {
+    let taskInput = document.getElementById("new-task");
+    let addButton = document.getElementsByTagName("button")[0];
+    let incompleteTaskHolder = document.getElementById("incomplete-tasks");
+    let completedTasksHolder = document.getElementById("completed-tasks");
+  }
+  AddTask(TaskTitle) {
+    let createNewTaskElement = function (TaskTitle) {
+      let listItem = document.createElement("li");
 
-            let listItem=document.createElement("li");
-        
-            let checkBox=document.createElement("input");
+      let checkBox = document.createElement("input");
 
-            let label=document.createElement("label");
+      let label = document.createElement("label");
 
-            let editInput=document.createElement("input");
+      let editInput = document.createElement("input");
 
-            let editButton=document.createElement("button");
-        
-            let deleteButton=document.createElement("button");
-        
-            label.innerText=TaskTitle;
-        
-            checkBox.type="checkbox";
-            editInput.type="text";
-        
-            editButton.innerText="Edit";
-            editButton.className="edit";
-            deleteButton.innerText="Delete";
-            deleteButton.className="delete";
-        
-            listItem.appendChild(checkBox);
-            listItem.appendChild(label);
-            listItem.appendChild(editInput);
-            listItem.appendChild(editButton);
-            listItem.appendChild(deleteButton);
-            AddTask(entry);
-            return listItem;
-        }
-    } 
-     AddTask = (AddTask) => {
-        this.addButton.addEventListener("click", (event) => {
-          event.preventDefault();
-          if (this.inputValue.value == "") alert("Your Input is Empty");
-          else {
-            AddTask(this.taskInput.value);
-          }
-          this.inputValue.value = "";
-        });
-      };
+      let editButton = document.createElement("button");
 
-      editTask = (editTask) => {
-        this.addButton.addEventListener("click", (event) => {
-          event.preventDefault();
-            let listItem=this.parentNode;
-            editTask(listItem);
-            let listItem1=this.parentNode;
-            TaskStatusChange(listItem1);
-          this.inputValue.value = "";
-        });
-      };
+      let deleteButton = document.createElement("button");
 
-      DeleteTask = (DeleteTask) => {
-        this.addButton.addEventListener("click", (event) => {
-          event.preventDefault();
-            let listItem=this.parentNode;
-            let ul = listItem.parentNode;
-            ul.removeChild(listItem);
-            DeleteTask(listItem);
-          this.inputValue.value = "";
-        });
-      };
-    
+      label.innerText = TaskTitle;
+
+      checkBox.type = "checkbox";
+      editInput.type = "text";
+
+      editButton.innerText = "Edit";
+      editButton.className = "edit";
+      deleteButton.innerText = "Delete";
+      deleteButton.className = "delete";
+
+      listItem.appendChild(checkBox);
+      listItem.appendChild(label);
+      listItem.appendChild(editInput);
+      listItem.appendChild(editButton);
+      listItem.appendChild(deleteButton);
+      AddTask(entry);
+      return listItem;
+    };
+  }
+  AddTask = (AddTask) => {
+    this.addButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      if (this.inputValue.value == "") alert("Your Input is Empty");
+      else {
+        AddTask(this.taskInput.value);
+      }
+      this.inputValue.value = "";
+    });
+  };
+
+  editTask = (editTask) => {
+    this.addButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      let listItem = this.parentNode;
+      editTask(listItem);
+      let listItem1 = this.parentNode;
+      TaskStatusChange(listItem1);
+      this.inputValue.value = "";
+    });
+  };
+
+  DeleteTask = (DeleteTask) => {
+    this.addButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      let listItem = this.parentNode;
+      let ul = listItem.parentNode;
+      ul.removeChild(listItem);
+      DeleteTask(listItem);
+      this.inputValue.value = "";
+    });
+  };
 }
 export default View;
