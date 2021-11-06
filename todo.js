f02da52f4637ca9701cc330c3ef9413170228171
erfan <69966847+erfanbiabanpoor@users.noreply.github.com>
2021-11-06 23:48:27 +0330

    ssss

--- todo.js
+++ todo.js
@@ -1,24 +1,51 @@
-function check() {
-  let item = document.getElementById("input").value;
-  if (item == "" || isNaN(item) !== true) {
-    alert(" input your task ");
+let taskDataBase = [];
+let status = "all";
+let countId = 0;
+let filterDefault = document.getElementsByClassName(".default");
+let saveTaskDataBase = localStorage.getItem("Todos");
+let saveId = localStorage.getItem("Id");
+let saveStatus = localStorage.getItem("Status");
+
+if (saveTaskDataBase && saveId) {
+  localStorage.setItem("Todos", JSON.stringify(taskDataBase));
+  let todos = JSON.parse(saveTaskDataBase);
+  taskDataBase = todos;
+  localStorage.setItem("Id", countId);
+  countId = +saveId + 1;
+  status = saveStatus;
+} else {
+  taskDataBase = [];
+  countId = 0;
+}
+
+function addTask() {
+  const inputValue = document.getElementById("input");
+  if (inputValue.value == "") {
+    alert("Invalid Task !");
+    return;
+  } else {
+    dataBase(inputValue.value);
+    renderPage();
+    inputValue.value = "";
   }
 }
-let input = document.getElementById("button");
-input.addEventListener("click", addtask);
-function addtask() {
-  let item = document.getElementById("input").value;
-  let li = document.createElement("li");
-  let text = document.createTextNode(item);
-  li.appendChild(text);
-  let ul = document.getElementById("list");
-  ul.appendChild(li);
+
+function dataBase(text) {
+  let taskObj = {
+    text: text,
+    completeStatus: false,
+    id: countId,
+    edit: false,
+  };
+  countId++;
+  taskDataBase.push(taskObj);
 }
+
 function renderPage() {
-  const TodoList = document.getElementById(".list");
+  const TodoList = document.getElementsByClassName("list");
   TodoList.innerHTML = "";
+
   const filteredDataBase = taskDataBase.filter((task) => {
-    let status;
     switch (status) {
       case "all": {
         filterDefault.textContent = "All";
@@ -42,7 +69,26 @@ function renderPage() {
       }
     }
   });
+  filteredDataBase.forEach(function (taskArry) {
+    const { id, completeStatus, text, edit } = taskArry;
+    const Task = document.createElement("li");
+    Task.classList.add("li-item");
+    Task.textContent = text;
+    Task.id = `${id}`;
+    localStorage.setItem("Id", id);
+    if (completeStatus) {
+      Task.classList.remove("complete-btn");
+      Task.classList.add("complete-status");
+    }
+    if (edit) {
+      Task.contentEditable = true;
+    }
+    createButton(Task);
+    TodoList.appendChild(Task);
+  });
+  localStorage.setItem("Todos", JSON.stringify(taskDataBase));
 }
+
 function createButton(Task) {
   const deleteButton = document.createElement("button");
   const editButton = document.createElement("button");
@@ -69,25 +115,32 @@ function createButton(Task) {
   });
   Task.appendChild(completeButton);
 }
+
 function editTask(event) {
   event.preventDefault();
   const editButton = event.target;
   const li = editButton.parentElement;
+  const id = +li.id;
 
   for (let task of taskDataBase) {
-    task.edit = !task.edit;
-    task.text = li.childNodes[0].nodeValue;
-    break;
+    if (task.id === id) {
+      task.edit = !task.edit;
+      task.text = li.childNodes[0].nodeValue;
+      break;
+    }
   }
+  renderPage();
 }
-renderPage();
 
 function deleteTask(event) {
   event.preventDefault();
   const deleteButton = event.target;
   const li = deleteButton.parentElement;
+  const id = +li.id;
 
-  taskDataBase = taskDataBase.filter((task) => {});
+  taskDataBase = taskDataBase.filter((task) => {
+    return task.id !== id;
+  });
   renderPage();
 }
 
@@ -95,18 +148,21 @@ function completeTask(event) {
   event.preventDefault();
   const completeButton = event.target;
   const li = completeButton.parentElement;
+  const id = +li.id;
 
   for (let task of taskDataBase) {
-    task.completeStatus = !task.completeStatus;
-    break;
+    if (task.id === id) {
+      task.completeStatus = !task.completeStatus;
+      break;
+    }
   }
+  renderPage();
 }
-renderPage();
 
 function filterTask() {
-  const filterElement = document.getElementsByClassName("default")[0];
+  const filterElement = document.getElementsByClassName("filter")[0];
   const filterELChild = filterElement.value.toLowerCase();
-  let status;
+  console.log(filterELChild);
   status = filterELChild;
 
   localStorage.setItem("Status", status);
